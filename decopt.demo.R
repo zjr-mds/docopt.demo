4cf8b91c23ddd94768ad787b1912cb87ba1d643c
Junrong Zhu <junrongz@student.ubc.ca>
2021-11-18 13:00:59 -0800

    arg3 changed

--- decopt.demo.R
+++ decopt.demo.R
@@ -2,7 +2,7 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
