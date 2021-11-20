4ac63ab16f3159bc1f5328103c45c005a145e15c
Junrong Zhu <junrongz@student.ubc.ca>
2021-11-19 19:09:03 -0800

    add demo.py with additional optional positional arg4

--- docopt.demo.py
+++ docopt.demo.py
@@ -1,5 +1,5 @@
-# author: Junrong Zhu
-# date: 2021-11-19
+# author: Tiffany Timbers
+# date: 2020-01-15
 
 """This script prints out docopt args.
 Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
@@ -7,17 +7,10 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[<arg4>]   Takes any value (an optional positional option)
 """ 
 
 from docopt import docopt
 opt = docopt(__doc__)
 print(opt)
-
-def main(opt):
-    print(opt)
-    print(type(opt))
-    print(opt['<arg4>'])
-
-if __name__ == "__main__":
-    main(opt)
+print(type(opt))
+print(opt['<arg4>'])
