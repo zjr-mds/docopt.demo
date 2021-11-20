e8dd401c5860971622fe03f58835a94042c8fb0b
Junrong Zhu <junrongz@student.ubc.ca>
2021-11-19 18:55:09 -0800

    add arg4 positional argument

--- docopt.demo.R
+++ docopt.demo.R
@@ -1,5 +1,5 @@
 # author: Junrong Zhu
-# date: 2020-01-15
+# date: 2021-11-19
 
 "This script prints out docopt args.
 Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
@@ -7,16 +7,13 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[--arg4=<arg4>]   Takes any value (this is an optional option)
+[<arg4>]   Takes any value (this is an optional positional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
 
-main <- function(opt) {
-    print(opt)
-    print(typeof(opt)) 
-    print(opt$arg4)
-}
-main(opt)
+print(opt)
+print(typeof(opt))
+print(opt$arg4)
 
