9f653487b3e8843cdecc9136e8b009b4f527d4bc
Junrong Zhu <junrongz@student.ubc.ca>
2021-11-19 18:56:24 -0800

    add arg4 optional positional argument

--- docopt.demo.R
+++ docopt.demo.R
@@ -7,7 +7,7 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[<arg4>]   Takes any value (this is an optional positional option)
+[<arg4>]   Takes any value (an optional positional option)
 " -> doc
 
 library(docopt)
